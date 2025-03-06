3dc330cbfbf781a47f2c06e26fd83ec51a26a859
Manreet Thind <Mthind2315@conestogac.on.ca>
2025-03-06 16:02:07 -0500

    function log_event completed

--- DX/src/DxUtils.c
+++ DX/src/DxUtils.c
@@ -18,3 +18,16 @@ int attach_shared_memory() {
 
     return (shm_id == -1) ? -1 : shm_id;
 }
+
+// Log events to dx.log
+void log_event(const char *message) {
+    FILE *log_file = fopen(LOG_FILE, "a");
+    if (log_file) {
+        time_t now = time(NULL);
+        struct tm *local_time = localtime(&now);
+        char time_str[20];
+        strftime(time_str, sizeof(time_str), "%Y-%m-%d %H:%M:%S", local_time);
+        fprintf(log_file, "[%s] : %s\n", time_str, message);
+        fclose(log_file);
+    }
+}
